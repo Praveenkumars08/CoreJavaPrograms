7eb7af957c9f074ac521c44df2a3db1b42d8705d
unknown <praveenkumar619.sp@gmail.com>
2022-10-25 11:45:33 +0530

    if else example 01

---
+++ src/com/java/basics/IfElseExample01.java
@@ -0,0 +1,24 @@
+package com.java.basics;
+
+public class IfElseExample01 {
+	
+	public static void main(String[] args) {
+		
+		if(10==20) {
+			System.out.println("Both the numbers are equal");
+		}
+		
+		else {
+			System.out.println("Both the numbers are not equal");
+		}
+		
+	}
+
+}
+
+/**
+ * In the above example, we know that 10 is not equal to 20.
+ * No matter how many times we execute it, it will go to the else part only.
+ * It's not a good practice of writing the code like this. Instead of that
+ * we need to save it in the variable.
+ */
