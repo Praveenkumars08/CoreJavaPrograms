608d739f355d183efc4e7ddca846698a8a068cb5
unknown <praveenkumar619.sp@gmail.com>
2022-11-02 18:37:36 +0530

    adding arrays example

---
+++ src/com/java/arrays/ArraysExample.java
@@ -0,0 +1,10 @@
+package com.java.arrays;
+
+public class ArraysExample {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+	}
+
+}
--- src/com/java/basics/DoWhileLoopExample.java
+++ src/com/java/basics/DoWhileLoopExample.java
@@ -7,7 +7,8 @@ public class DoWhileLoopExample {
 		/**
 		 * Explanation : In do while loop, first it will execute the statement at least once, and then
 		 * it will check the condition. Even though if the condition is false the statement will be
-		 * executed once. 
+		 * executed once. It is mandatory to either increment or decrement the value or else the
+		 * condition will always be true and stuck in an infinite loop.
 		 */
 		
 		int a = 10;
